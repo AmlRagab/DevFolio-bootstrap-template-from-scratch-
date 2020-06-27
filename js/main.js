6f02da8e9c50f79e7c430ef511371b6ebd4a2ced
aml ragab <amlragab81@gmail.com>
2020-06-27 02:31:59 +0200

    modify

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
-  let aboutscroll = $("#about").offset().top;
   $(window).scroll(function () {
     let usersroll = $(window).scrollTop();
+let abt =$("#about").offset().top;
+    if (usersroll >="50") {
 
-    if (usersroll >= aboutscroll) {
+      $("nav").css("backgroundColor", "fff")
+      $("#navbar").css("boxShadow", "0px 12px 30px -13px rgba(0,0,0,0.4)");
 
-      $("#btnup").fadeIn(500);
-      $("#navbar").css("backgroundColor", "rgba(0,0,0,0.8)")
+      $("#navbar a").css("color", "#007bff")
+      if(usersroll>abt)
+      {
+        $("#btnup").fadeIn(500);
+      
+      }
+
     }
 
     else {
       $("#btnup").fadeOut(500);
       $("#navbar").css("backgroundColor", "transparent")
+      $("#navbar").css("boxShadow", "none");
+      $("nav a").css("color", "white");
     }
   });
 
@@ -20,7 +29,7 @@
 
 
 
-$("#home a").click(function () {
+$("#navbar a").click(function () {
   let idOfLink = $(this).attr("href")
   let  secoffset = $(idOfLink).offset().top;
   $("html,body").animate({ scrollTop:secoffset }, 1000)
